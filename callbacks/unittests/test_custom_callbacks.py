ce2983fab79a2fdd315a58bfdb1b38ada115fa53
SvenWarnke <sven.warnke@connyun.com>
2020-12-14 15:56:12 +0100

    Test that callbacks dont fail

--- callbacks/unittests/test_custom_callbacks.py
+++ callbacks/unittests/test_custom_callbacks.py
@@ -1,4 +1,6 @@
+import pytest
 import numpy as np
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from callbacks import custom_callbacks
@@ -7,13 +9,19 @@ from callbacks import custom_callbacks
 NUM_CLASSES = 10
 INPUT_SHAPE = (28, 28, 1)
 BATCH_SIZE = 128
-EPOCHS = 1
+EPOCHS = 5
 
 
 def get_data():
 
     # the data, split between train and test sets
     (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+    x_train = x_train[:100]
+    y_train = y_train[:100]
+
+    x_test = x_test[:10]
+    y_test = y_test[:10]
 
     # Scale images to the [0, 1] range
     x_train = x_train.astype("float32") / 255
@@ -26,7 +34,7 @@ def get_data():
     y_train = keras.utils.to_categorical(y_train, NUM_CLASSES)
     y_test = keras.utils.to_categorical(y_test, NUM_CLASSES)
 
-    return x_train, y_train
+    return (x_train, y_train), (x_test, y_test)
 
 
 def get_model():
@@ -47,13 +55,20 @@ def get_model():
     return model
 
 
-x_train, y_train = get_data()
-model = get_model()
-model.fit(
-    x_train,
-    y_train,
-    batch_size=BATCH_SIZE,
-    epochs=EPOCHS,
-    validation_split=0.1,
-    callbacks=[custom_callbacks.HelloWorldCallback(file_writer="bla")]
-)
+def test_doesnt_fail():
+    (x_train, y_train), (x_test, y_test) = get_data()
+    model = get_model()
+
+    file_writer_cm = tf.summary.create_file_writer('logs/cm')
+    model.fit(
+        x_train,
+        y_train,
+        validation_data=(x_test, y_test),
+        batch_size=BATCH_SIZE,
+        epochs=EPOCHS,
+        callbacks=[
+            custom_callbacks.HelloWorldCallback(file_writer="bla"),
+            custom_callbacks.ConfusionMatrixCallback(file_writer=file_writer_cm, val_data=(x_test, y_test)),
+            custom_callbacks.SaveMisslabeledImages(file_writer=file_writer_cm, validation_data=(x_test, y_test))
+        ]
+    )
